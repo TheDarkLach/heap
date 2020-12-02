2c3f34a9725adff619c7122b4ea183bc53ca2fb2
TheDarkLach <11thunderstorm11@gmail.com>
2020-12-01 20:36:55 -0800

    yes 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
-
+#include <iostream>
+#include <cstring>
+#include <stdlib.h>
+#include <fstream>
+#include <cmath>
 
 using namespace std;
+
+void parseIn(int*& parsed, char input[], int &counter);
+void heapify(int*& arr, int n, int i);
+void buildHeap(int*& arr, int n);
+void printHeap(int* arr, int n);
+void sortHeap(int* arr, int n);
+void visualPrint(int* arr, int n);
 
 int main()
 {
